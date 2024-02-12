61727af4c9867c46aebde5db55612127a894e0d4
Hermes Santos <hermessantos995@gmail.com>
2024-02-12 11:52:33 -0300

    listing current php version in list command

--- pvm.sh
+++ pvm.sh
@@ -2,17 +2,35 @@
 #!/bin/bash
 
 flag="$1"
-php_version="$2"
+secong_flag="$2"
 
 arguments="list, -l, use [PHP_Version], --help, -h"
 directory="/usr/bin"
-update_alternatives="sudo update-alternatives --set php /usr/bin/php$php_version"
+update_alternatives="sudo update-alternatives --set php /usr/bin/php$secong_flag"
 actual_verison="PVM - PHP Version Manager\n 1.0.0"
+warn_no_args="No arguments given. Type pvm --help or pvm -h to see all the commands"
+install_php_version="sudo apt-get install php$secong_flag"
+
+install_ppa_for_php="sudo add-apt-repository ppa:ondrej/php"
+verify_ppa() {
+	ls /etc/apt/sources.list.d/ | grep ondrej
+}
 
 list_php_versions() {
-	ls $directory | grep -oP 'php\d+\.\d+' | sed 's/php//'
+	ls $directory | grep -oP 'php\d+\.\d+' | sed 's/php//' | while read -r version; do
+		if php -v | grep -oP 'PHP \K[0-9]+\.[0-9]+\.[0-9]+' | grep -q "$version"; then
+			echo $version "* (Current using)"
+		else
+			echo $version
+		fi
+	done
 }
-warn_no_args="No arguments given. Type pvm --help or pvm -h to see all the commands"
+get_current_php_version() {
+	php -v | grep -oP 'PHP \K[0-9]+\.[0-9]+\.[0-9]+'
+}
+install_ppa_for_php() {
+	sudo add-apt-repository ppa:ondrej/php
+}
 
 verify_flags() {
 	if [ "$flag" = "" ]; then
@@ -29,10 +47,20 @@ verify_flags() {
 		echo "PHP Versions"
 		list_php_versions
 	elif [ "$flag" = "use" ]; then
-		if [ "$php_version" = "" ]; then
+		if [ "$secong_flag" = "" ]; then
 			echo "$warn_no_args"
 		else
 			$update_alternatives
+		fi
+	elif [ "$flag" = "-i" ]; then
+		ppa_files=$(verify_ppa)
+		if [ "$secong_flag" = "" ]; then
+			echo "$warn_no_args"
+		elif [ -n "$ppa_files" ]; then
+			install_php_version
+		else
+			echo "Installing PPA"
+			install_ppa_for_php
 		fi
 	fi
 }
